cdeb4e3098eb822e9016383e1e1425440a541bf5
FacenZHOU <facenzhou@gmail.com>
2023-04-09 15:01:38 +0200

    changed test_endpoint.py

--- test_endpoints.py
+++ test_endpoints.py
@@ -39,27 +39,27 @@ def test_post_predict_less():
         "native_country": "United-States"
     })
     assert r.status_code == 200
-    assert r.json() == {"Income prediction": ">50K"}
+    assert r.json() == {"Income prediction": "<=50K"}
     
 
 def test_post_predict_more():
     """Test predict funtion when income is more than 50K"""
 
     r = client.post("/predict-income", json={
-        "age": 28,
+        "age": 37,
         "workclass": "Private",
-        "fnlgt": 183175,
+        "fnlgt": 280464,
         "education": "Some-college",
         "education_num": 10,
-        "marital_status": "Divorced",
-        "occupation": "Adm-clerical",
-        "relationship": "Not-in-family",
-        "race": "White",
-        "sex": "Female",
+        "marital_status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "Black",
+        "sex": "Male",
         "capital_gain": 0,
         "capital_loss": 0,
-        "hours_per_week": 40,
+        "hours_per_week": 80,
         "native_country": "United-States"
     })
     assert r.status_code == 200
-    assert r.json() == {"Income prediction": "<=50K"}
+    assert r.json() == {"Income prediction": ">50K"}
